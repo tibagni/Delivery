2b286cace3f386120334e68d73ca871e679225c0
Tiago Bagni <tibagni@gmail.com>
2012-09-09 11:58:44 -0300

    Android Project
    Criação do projeto android

---
+++ Android/gen/com/delivery/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.delivery.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Android/src/com/delivery/android/OrderListActivity.java
@@ -0,0 +1,44 @@
+package com.delivery.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+public class OrderListActivity extends FragmentActivity
+        implements OrderListFragment.Callbacks {
+
+    private boolean mTwoPane;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_list);
+
+        if (findViewById(R.id.order_detail_container) != null) {
+            mTwoPane = true;
+            ((OrderListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.order_list))
+                    .setActivateOnItemClick(true);
+        }
+    }
+
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            Bundle arguments = new Bundle();
+            arguments.putString(OrderDetailFragment.ARG_ITEM_ID, id);
+            OrderDetailFragment fragment = new OrderDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.order_detail_container, fragment)
+                    .commit();
+
+        } else {
+            Intent detailIntent = new Intent(this, OrderDetailActivity.class);
+            detailIntent.putExtra(OrderDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
