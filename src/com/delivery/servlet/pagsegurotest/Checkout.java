5aeb28a5f23fe159f40e0758acc7c5edbc5e0cb4
Tiago Bagni <tibagni@gmail.com>
2012-09-10 00:27:12 -0300

    Pag Seguro test 2
    Continuação do servlet de teste do pag seguro (Simulação do servidor do
    PagSeguro)

--- src/com/delivery/servlet/pagsegurotest/Checkout.java
+++ src/com/delivery/servlet/pagsegurotest/Checkout.java
@@ -1,14 +1,22 @@
 package com.delivery.servlet.pagsegurotest;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.ParserConfigurationException;
 
-import org.xml.sax.SAXException;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 
 import br.com.uol.pagseguro.domain.PaymentRequest;
 import br.com.uol.pagseguro.xmlparser.PaymentParser;
@@ -23,18 +31,57 @@ public class Checkout extends HttpServlet {
 			throws ServletException, IOException {
 
 		PaymentRequest paymentRequest = null;
+		InputStream is = req.getInputStream();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		IOUtils.copy(is, baos);
+		byte[] xmlBytes = baos.toByteArray();
+		String paymentCode;
 		try {
-			paymentRequest = PaymentParser.readPaymentRequestXml(req.getInputStream());
+			paymentRequest = PaymentParser.readPaymentRequestXml(new ByteArrayInputStream(xmlBytes));
 			Logger.debug(paymentRequest.toString());
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
+
+			// Salva o paymentRequest em um arquivo xml
+
+			//Primeiro gera o nome do arquivo - que sera o codigo do pagamento
+			Random r = new Random();
+			byte[] randomBytes = new byte[5];
+			r.nextBytes(randomBytes);
+
+			// Cria um hash combinando o xml com alguns bytes aleatorios (para que seja unico)
+			byte[] nameBytes = new byte[xmlBytes.length + randomBytes.length];
+			System.arraycopy(xmlBytes, 0, nameBytes, 0, xmlBytes.length);
+			System.arraycopy(randomBytes, 0, nameBytes, xmlBytes.length, randomBytes.length);
+			paymentCode = hashName(nameBytes);
+
+			File f = new File(req.getServletContext().getRealPath("/testePagSeguro/" + paymentCode));
+			OutputStream fos = new FileOutputStream(f);
+			fos.write(xmlBytes);
+			fos.close();
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// Qualquer excessao tratamos como erro
+			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return;
 		}
 
-		resp.getWriter().write("<checkout><code>" + 123 + "</code></checkout>");
+		resp.getWriter().write("<checkout><code>" + paymentCode + "</code></checkout>");
 		resp.setStatus(HttpServletResponse.SC_OK);
 	}
+
+	private String hashName(byte[] bytes) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(bytes);
+		byte[] hashMd5 = md.digest();
+
+		StringBuilder s = new StringBuilder();
+		   for (int i = 0; i < hashMd5.length; i++) {
+		       int parteAlta = ((hashMd5[i] >> 4) & 0xf) << 4;
+		       int parteBaixa = hashMd5[i] & 0xf;
+		       if (parteAlta == 0) s.append('0');
+		       s.append(Integer.toHexString(parteAlta | parteBaixa));
+		   }
+		   return s.toString();
+	}
+
+
 }
